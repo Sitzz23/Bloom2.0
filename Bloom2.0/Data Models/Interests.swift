37dc599a52c606a57aee29a468b682fedd5554ee
Sitanshu Pokalwar <sitz2309@gmal.com>
2023-03-20 23:17:00 +0530

    splash screen and interest screen

---
+++ Bloom2.0/Data Models/Interests.swift
@@ -0,0 +1,10 @@
+//
+//  dataModel.swift
+//  Bloom2.0
+//
+//  Created by Sitanshu Pokalwar on 17/03/23.
+//
+
+import Foundation
+
+
--- Bloom2.0/ViewController.swift
+++ Bloom2.0/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var toHome: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    
 
 }
 
